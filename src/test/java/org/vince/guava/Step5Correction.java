8b2dcdb565987a080f4c9cee078c628ae1cd719b
vincent <vincent314@hotmail.fr>
2015-10-08 23:52:54 +0200

    feat(test): ajoute le step5, lecture et traitement du dictionnaire de Kanji

--- src/test/java/org/vince/guava/Step5Correction.java
+++ src/test/java/org/vince/guava/Step5Correction.java
@@ -1,6 +1,8 @@
 package org.vince.guava;
 
 import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
 import com.google.common.collect.*;
 import org.junit.Test;
 import org.vince.guava.mm.EnglishWord;
@@ -11,6 +13,7 @@ import org.vince.guava.mm.Meaning;
 import javax.xml.bind.JAXB;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,18 +29,18 @@ public class Step5Correction {
             KanjiDic.class
         );
 
-
         assertEquals(6355, kanjiDic.getKanji().size());
 
-
         ImmutableMap<String, Collection<EnglishWord>> index = FluentIterable.from(kanjiDic.getKanji())
+            .filter(new Predicate<Kanji>() {
+                @Override
+                public boolean apply(Kanji kanji) {
+                    return kanji.getMeaning() != null;
+                }
+            })
             .transformAndConcat(new Function<Kanji, Iterable<EnglishWord>>() {
                 @Override
                 public Iterable<EnglishWord> apply(Kanji kanji) {
-                    if (kanji.getMeaning() == null) {
-                        return Lists.newArrayList();
-                    }
-
                     return Iterables.transform(kanji.getMeaning(), new Function<Meaning, EnglishWord>() {
                         @Override
                         public EnglishWord apply(Meaning meaning) {
@@ -53,6 +56,28 @@ public class Step5Correction {
                 }
             }).asMap();
 
-        assertEquals(13, index.get("quiet").size());
+        // Cas de test "car"
+        Collection<EnglishWord> carResult = index.get("car");
+        assertEquals(1, carResult.size());
+        assertEquals("車", carResult.iterator().next().getKanji().getCharacter());
+
+        // Cas de test
+        Collection<EnglishWord> houseResult = index.get("house");
+
+        assertEquals(10, houseResult.size());
+        assertEquals(
+            "宇,屋,家,舎,宅,内,房,厦,廈,舍",
+            Joiner.on(",").join(
+                Iterables.transform(
+                    houseResult,
+                    new Function<EnglishWord, String>() {
+                        @Override
+                        public String apply(EnglishWord englishWord) {
+                            return englishWord.getKanji().getCharacter();
+                        }
+                    }
+                )
+            )
+        );
     }
 }
