f2fe48d35cc4908cb0d7a41f3a7be9fc89ee18bc
vincent <vincent314@hotmail.fr>
2015-10-10 18:21:37 +0200

    feat(test): ajoute l'implémentation vanilla du Step5 + diagrames UML du step 5

--- src/test/java/org/vince/guava/Step5.java
+++ src/test/java/org/vince/guava/Step5.java
@@ -1,13 +1,18 @@
 package org.vince.guava;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.vince.guava.mm.EnglishWord;
+import org.vince.guava.mm.Kanji;
 import org.vince.guava.mm.KanjiDic;
+import org.vince.guava.mm.Meaning;
 
 import javax.xml.bind.JAXB;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by vincent on 06/10/15.
@@ -21,8 +26,46 @@ public class Step5 {
             KanjiDic.class
         );
 
-
-
         assertEquals(6355,kanjiDic.getKanji().size());
+
+        HashMap<String, List<EnglishWord>> index = new HashMap<String, List<EnglishWord>>();
+
+        // Indexer dans un Map un Character Kanji avec la liste de ses sens
+        for (Kanji kanji : kanjiDic.getKanji()) {
+            if(kanji.getMeaning() == null) continue;
+
+            for (Meaning meaning : kanji.getMeaning()) {
+                List<EnglishWord> meanings = index.get(meaning.getValue());
+                if (meanings == null) {
+                    meanings = new ArrayList<EnglishWord>();
+                    index.put(meaning.getValue(), meanings);
+                }
+                meanings.add(new EnglishWord(kanji, meaning.getValue()));
+            }
+        }
+
+        // Cas de test "car"
+        List<EnglishWord> carResult = index.get("car");
+        assertEquals(1, carResult.size());
+        assertEquals("車", carResult.get(0).getKanji().getCharacter());
+
+        // Cas de test sur "house"
+        Collection<EnglishWord> houseResult = index.get("house");
+        StringBuilder buffer = new StringBuilder();
+        boolean isFirst = true;
+        for (EnglishWord englishWord : houseResult) {
+            if(!isFirst) {
+                buffer.append(",");
+            }
+            buffer.append(englishWord.getKanji().getCharacter());
+            isFirst = false;
+
+        }
+
+        assertEquals(10, houseResult.size());
+        assertEquals(
+            "宇,屋,家,舎,宅,内,房,厦,廈,舍",
+            buffer.toString()
+        );
     }
 }
--- src/test/java/org/vince/guava/Step5Java8.java
+++ src/test/java/org/vince/guava/Step5Java8.java
@@ -1,10 +1,8 @@
 package org.vince.guava;
 
-import com.google.common.base.Function;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Iterables;
 import org.junit.Test;
 import org.vince.guava.mm.EnglishWord;
+import org.vince.guava.mm.Kanji;
 import org.vince.guava.mm.KanjiDic;
 
 import javax.xml.bind.JAXB;
@@ -15,9 +13,6 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by vincent on 06/10/15.
- */
 public class Step5Java8 {
 
     @Test
@@ -29,6 +24,9 @@ public class Step5Java8 {
 
         assertEquals(6355, kanjiDic.getKanji().size());
 
+        List<Kanji> list = kanjiDic.getKanji().stream().filter((kanji -> "文".equals(kanji.getCharacter()))).collect(Collectors.toList());
+
+        // Indexer Kanji --> sens
         Map<String, List<EnglishWord>> index = kanjiDic.getKanji()
             .stream()
             .filter((kanji -> kanji.getMeaning() != null))
@@ -50,17 +48,9 @@ public class Step5Java8 {
         assertEquals(10, houseResult.size());
         assertEquals(
             "宇,屋,家,舎,宅,内,房,厦,廈,舍",
-            Joiner.on(",").join(
-                Iterables.transform(
-                    houseResult,
-                    new Function<EnglishWord, String>() {
-                        @Override
-                        public String apply(EnglishWord englishWord) {
-                            return englishWord.getKanji().getCharacter();
-                        }
-                    }
-                )
-            )
+            houseResult.stream()
+                .map(englishWord -> englishWord.getKanji().getCharacter())
+                .collect(Collectors.joining(","))
         );
     }
 }
