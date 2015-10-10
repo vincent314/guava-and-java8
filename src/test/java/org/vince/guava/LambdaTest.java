22c892ffd742dd94a46d6195004f764f2f058ead
vincent <vincent314@hotmail.fr>
2015-10-11 00:46:05 +0200

    fix(test): Utilise plutôt UnaryOperator pour le LambdaTest

--- src/test/java/org/vince/guava/LambdaTest.java
+++ src/test/java/org/vince/guava/LambdaTest.java
@@ -2,24 +2,24 @@ package org.vince.guava;
 
 import org.junit.Test;
 
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import static org.junit.Assert.assertEquals;
 
 public class LambdaTest {
 
     @Test
-    public void testLamda(){
-        Function<String,String> toUpper1 = new Function<String,String>(){
+    public void testLamda() {
+        UnaryOperator<String> toUpper1 = new UnaryOperator<String>() {
             @Override
             public String apply(String source) {
                 return source.toUpperCase();
             }
         };
 
-        Function<String, String> toUpper2 = (source) -> source.toUpperCase();
+        UnaryOperator<String> toUpper2 = (source) -> source.toUpperCase();
 
-        Function<String, String> toUpper3 = String::toUpperCase;
+        UnaryOperator<String> toUpper3 = String::toUpperCase;
 
         assertEquals("HELLO WORLD", toUpper1.apply("hello world"));
         assertEquals("HELLO WORLD", toUpper2.apply("hello world"));
